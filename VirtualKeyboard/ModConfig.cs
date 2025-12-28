9b1c692d526a06ce7f94e66e1925ea96570d4b04
DoubleZ <zhangtongyuan.cpp@gmail.com>
2025-12-28 17:42:56 +0800

    rename

--- VirtualKeyboard/ModConfig.cs
+++ VirtualKeyboard/ModConfig.cs
@@ -7,7 +7,7 @@ namespace VirtualKeyboard
     {
         public int AboveMenu { get; set; } = 0;
         public float ButtonScale { get; set; } = 1.0f;
-        public string TriggerPath { get; set; } = "assets/togglebutton.png";
+        public string TogglePath { get; set; } = "assets/togglebutton.png";
         public Toggle vToggle { get; set; } = new Toggle((SButton)0, new Rect(Constants.TargetPlatform == GamePlatform.Android ? 96 : 36, 12, 64, 64));
         public List<VirtualButton> Buttons { get; set; } = new List<VirtualButton>{
               new VirtualButton((SButton) 80, new Pos(0, 0)),
